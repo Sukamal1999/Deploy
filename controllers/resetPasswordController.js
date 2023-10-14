3d601b2335fe3b68d6161a58cd80e1bc1355b6fc
Sukamal <sukamalmondal07@gmail.com>
2023-10-14 22:04:17 +0530

    report page modification

--- controllers/resetPasswordController.js
+++ controllers/resetPasswordController.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcrypt");
 const Sib = require("sib-api-v3-sdk");
 const { v4: uuidv4 } = require("uuid");
 const saltRounds = 10;
+require('dotenv').config()
 
 const hashPassword = async (password) => {
   return await bcrypt.hash(password, saltRounds);
@@ -12,14 +13,11 @@ const hashPassword = async (password) => {
 
 exports.forgotPasswordPage = async (req, res, next) => {
   try {
-    res
-      .status(200)
-      .sendFile(
-        path.join(__dirname, "../", "public", "views", "forgot.html")
-      );
+    res.sendFile(path.join(__dirname, "../public/views/forgot.html"));
   } catch (error) {
     console.log(error);
-  }  
+    return res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 exports.sendMail = async (req, res, next) => {
@@ -27,27 +25,26 @@ exports.sendMail = async (req, res, next) => {
     const email = req.body.email;
     const requestId = uuidv4();
 
-    const recepientEmail = await User.findOne({ where: { email: email } });
+    const recipientEmail = await User.findOne({ where: { email: email } });
 
-    if (!recepientEmail) {
+    if (!recipientEmail) {
       return res
         .status(404)
-        .json({ message: "Please provide the registered email!" });
+        .json({ message: "Please provide a registered email!" });
     }
 
     const resetRequest = await ResetPassword.create({
       id: requestId,
       isActive: true,
-      userId: recepientEmail.dataValues.id,
+      userId: recipientEmail.dataValues.id,
     });
 
     const client = Sib.ApiClient.instance;
     const apiKey = client.authentications["api-key"];
-    apiKey.apiKey = process.env.RESET_PASSWORD_API_KEY;
+    apiKey.apiKey = process.env.API_KEY;
     const transEmailApi = new Sib.TransactionalEmailsApi();
     const sender = {
-      email: "sukamalmondal07@gmail.com",
-      name: "Sukamal",
+      email: "sukamalmondal07@gmail.com", 
     };
     const receivers = [
       {
@@ -56,65 +53,59 @@ exports.sendMail = async (req, res, next) => {
     ];
     const emailResponse = await transEmailApi.sendTransacEmail({
       sender,
-      To: receivers,
+      to: receivers,
       subject: "Expense Tracker Reset Password",
       textContent: "Link Below",
-      htmlContent: `<h3>Hi! We got the request from you for reset the password. Here is the link below >>></h3>
-      <a href="http://localhost:3000/password/resetPasswordPage/{{params.requestId}}"> Click Here</a>`,
-      params: {
-        requestId: requestId,
-      },
+      htmlContent: `<h3>Hi! We received a request from you to reset your password. Here is the link below:</h3>
+      <a href="http://localhost:3000/password/resetPasswordPage/${requestId}"> Click Here</a>`,
     });
+    
     return res.status(200).json({
-      message:
-        "Link for reset the password is successfully send on your Mail Id!",
+      message: "A password reset link has been sent to your email address.",
     });
   } catch (error) {
-    console.log("error");
-    return res.status(409).json({ message: "failed changing password" });
+    console.error(error);
+    return res.status(500).json({ message: "Failed to send the reset password email" });
   }
 };
 
 exports.resetPasswordPage = async (req, res, next) => {
   try {
-    res
-      .status(200)
-      .sendFile(
-        path.join(__dirname, "../", "public", "views", "resetPassword.html")
-      );
+    res.sendFile(path.join(__dirname, "../public/views/resetPassword.html"));
   } catch (error) {
     console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
   }
 };
 
 exports.updatePassword = async (req, res, next) => {
   try {
-    const requestId = req.headers.referer.split("/");
+    const requestId = req.params.requestId; // Use params to get the request ID
     const password = req.body.password;
-    const checkResetRequest = await ResetPassword.findAll({
-      where: { id: requestId[requestId.length - 1], isActive: true },
+    const checkResetRequest = await ResetPassword.findOne({
+      where: { id: requestId, isActive: true },
     });
-    if (checkResetRequest[0]) {
-      const userId = checkResetRequest[0].dataValues.userId;
-      const result = await ResetPassword.update(
-        { isActive: false },
-        { where: { id: requestId } }
-      );
-      const newPassword = await hashPassword(password);
-      const user = await User.update(
-        { password: newPassword },
-        { where: { id: userId } }
-      );
-      return res
-        .status(200)
-        .json({ message: "Successfully changed password!" });
-    } else {
-      return res
-        .status(409)
-        .json({ message: "Link is already Used Once, Request for new Link!" });
+
+    if (!checkResetRequest) {
+      return res.status(409).json({ message: "Reset link is invalid or expired" });
     }
+
+    const userId = checkResetRequest.userId;
+
+    await ResetPassword.update(
+      { isActive: false },
+      { where: { id: requestId } }
+    );
+
+    const newPassword = await hashPassword(password);
+    await User.update(
+      { password: newPassword },
+      { where: { id: userId } }
+    );
+
+    return res.status(200).json({ message: "Password successfully changed" });
   } catch (err) {
-    console.log(err);
-    return res.status(409).json({ message: "Failed to change password!" });
+    console.error(err);
+    return res.status(500).json({ message: "Failed to change password" });
   }
 };
--- router/resetPasswordRouter.js
+++ router/resetPasswordRouter.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 const resetPasswordController = require("../controllers/resetPasswordController");
 
+// Route to display the forgot password page
 router.get("/forgotPasswordPage", resetPasswordController.forgotPasswordPage);
+
+// Route to display the reset password page with a requestId parameter
+router.get("/resetPasswordPage/:requestId", resetPasswordController.resetPasswordPage);
+
+// Route to send mail for password reset
 router.post("/sendMail", resetPasswordController.sendMail);
-router.get(
-  "/resetPasswordPage/:requestId",
-  resetPasswordController.resetPasswordPage
-);
+
+// Route to update the password
 router.post("/resetPassword", resetPasswordController.updatePassword);
 
 module.exports = router;
